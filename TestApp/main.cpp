bd2328fd18a932162e7ee410eb0feb381ea1683f
tunabolt <tunabolt.com@gmail.com>
2014-03-12 14:57:25 -0700

    Initial Commit

---
+++ TestApp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  TestApp
+//
+//  Created by Tuan Vo on 3/12/14.
+//  Copyright (c) 2014 Tuan Vo. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
